5cd85ff004dd064f9d956ce80fcadfaf9ca3f618
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-12-04 09:41:31 -0800

    added a welcome and main menu otions to cli

--- lib/dog_app/cli.rb
+++ lib/dog_app/cli.rb
@@ -1,7 +1,21 @@
 class CLI
 
   def start
-  puts "Hello World"
+   puts "Welcome"
+
+   puts "Loading..."
+
+   main_menu_options
   end 
+
+  def main_menu_options
+    puts "Type '1' to get a list of dog breeds."
+    puts "Type '2' to enter a breed name."
+    puts "Type 'exit' to exit the app."
+  end 
+
+  
+
+
   
 end 
