1c235154db9eccbb244a29429d5d8182b9d38739
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-12-04 09:49:21 -0800

    added main menu, invalid choice, and get input to cli

--- lib/dog_app/cli.rb
+++ lib/dog_app/cli.rb
@@ -12,10 +12,31 @@ class CLI
     puts "Type '1' to get a list of dog breeds."
     puts "Type '2' to enter a breed name."
     puts "Type 'exit' to exit the app."
+    main_menu
   end 
 
-  
+  def main_menu
+    input = get_input
+    if input == "1"
+      puts "Here is a list of all the dog breeds you can choose from."
+    elsif input == "2"
+      puts "Please enter a dog breed."
+    elsif input == "exit"
+    puts "Exiting the application."
+    else
+      invalid_choice
+      main_menu_options
+    end 
+  end 
 
+  def invalid_choice
+    puts "Invalid choice please try again."
+  end 
+
+  def get_input
+    puts "Enter a choice."
+    gets.strip
+  end 
 
   
 end 
